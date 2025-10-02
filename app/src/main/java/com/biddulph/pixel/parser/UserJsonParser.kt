dbc2f01db2f61b373e7b869f740ef220ec218711
Rich <biddulphdev@gmail.com>
2025-10-02 09:28:08 +0100

    Moved HTML decoding to allow tests to pass without mocking

--- app/src/main/java/com/biddulph/pixel/parser/UserJsonParser.kt
+++ app/src/main/java/com/biddulph/pixel/parser/UserJsonParser.kt
@@ -14,15 +14,23 @@ object UserJsonParser {
         val parsedUsers = (0 until items.length()).map { index ->
             val item = items.getJSONObject(index)
 
-            // ensure HTML encoding of display names is handled - for the purpose of this exercise just using a simple parser will suffice
             UserRemote(
                 user_id = item.getInt("user_id"),
-                display_name = HtmlCompat.fromHtml(item.getString("display_name"), HtmlCompat.FROM_HTML_MODE_COMPACT).toString(),
+                display_name = decodeHtml(item.getString("display_name")),
                 reputation = item.getInt("reputation"),
                 profile_image = item.optString("profile_image").ifEmpty { null }
             )
         }
 
         return UserRemoteResponse(items = parsedUsers)
+    }
+
+    // ensure HTML encoding of display names is handled - for the purpose of this exercise just using a simple parser will suffice
+    private fun decodeHtml(originalString: String): String {
+        return try {
+            HtmlCompat.fromHtml(originalString, HtmlCompat.FROM_HTML_MODE_COMPACT).toString()
+        } catch (_: Throwable) {
+            originalString
+        }
     }
 }
